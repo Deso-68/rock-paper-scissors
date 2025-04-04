e320b87210a4ff98245c0910a591b7baec32dd47
Deso-68 <174117432+Deso-68@users.noreply.github.com.>
2025-04-04 18:48:10 +0800

    Introduce getRandomInt function for computer choice selection

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // Rock, Paper, Scissors constant variables
-const ROCK = "rock" 
-const PAPER = "paper"
-const SCISSORS = "scissor"
+const ROCK = "rock"; 
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
