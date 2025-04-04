c0ff83a8295bb8cb6096231e2ae9c4ee77289213
Deso-68 <174117432+Deso-68@users.noreply.github.com.>
2025-04-04 18:58:31 +0800

    Implement getComputerChoice that returns rock, paper, scissor string

--- script.js
+++ script.js
@@ -8,17 +8,24 @@ const MIN_CHOICE = 1;
 const MAX_CHOICE = 4;
 
 
+
 function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
 
-function getcomputerChoice() {
-    randomInt = getRandomInt(1, 4);
+function getComputerChoice() {
+    randomInt = getRandomInt(MIN_CHOICE, MAX_CHOICE);
+    // console.log(randomInt);
+
     if(randomInt === 1) {
-        return 
+        return ROCK;
     }
-    
-    return;
+    else if(randomInt === 2) {
+        return PAPER;
+    }
+    else {
+        return SCISSORS;
+    }
 }
