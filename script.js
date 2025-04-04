afc03c4cc0d9fdc2a04baca111681d304d55eccd
Deso-68 <174117432+Deso-68@users.noreply.github.com.>
2025-04-04 18:51:06 +0800

    Define MIN_CHOICE and MAX_CHOICE constants

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
 
+// For getComputerChoice min and max value
+const MIN_CHOICE = 1;
+const MAX_CHOICE = 4;
+
 
 function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+function getcomputerChoice() {
+    randomInt = getRandomInt(1, 4);
+    if(randomInt === 1) {
+        return 
+    }
+    
+    return;
 }
